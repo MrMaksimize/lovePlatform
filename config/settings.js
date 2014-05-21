206283017bacba6cb0c0df4a361150f7ff88b0c7
Maksim Pecherskiy <max@maksimize.com>
2014-05-21 10:29:19 -0700

    Update settings file.

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,20 @@
 module.exports = {
-
+  title: 'LovePlatform',
+  heading: 'I Love LovePlatform Because...',
+  form: {
+    textLabel: 'Message',
+    textPlaceHolder: 'Message',
+    twitterLabel: '@',
+    twitterPlaceholder: 'Twitter',
+    submitButtonText: 'Submit'
+  },
+  notes: {
+    displayInitial: 9,
+    voting: true,
+    tweeting: true,
+  },
+  afterSubmit: {
+    popUpTweetPrompt: true
+  }
 };
 
